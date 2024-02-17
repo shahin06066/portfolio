dbb78d6a9a0eb6caee2741fe32ae44859b30272b
Md Shahin <153791843+shahin06066@users.noreply.github.com>
2024-02-17 15:39:05 +0600

    preloader has added

--- script.js
+++ script.js
@@ -37,6 +37,12 @@ menuIcon.onclick = () => {
 
 }
 
+//preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+loader.style.display ="none";
+});
+
 
 
 
